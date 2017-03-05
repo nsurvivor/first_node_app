122cb9a5799d776089ade8881712467788810f50
nightslacker <kmnsurvivor@gmail.com>
2017-03-06 00:30:47 +0530

    express installed so new dependency added to package.json

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ INDEX.js - keep this file as lean as possible
 //require('./app/index.js')
 //require('./app/read_input_CLI.js')
 //console.log('START AND END HERE ON INDEX 1')
-require('./app/http_index.js')
+require('./app/http_index.js') //to run another index file than the index.js
